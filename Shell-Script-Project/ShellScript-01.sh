4f93d1f0f668a0e8f64a8029a26599df3b344146
Mani Kevala <manikevala2001@gmail.com>
2025-06-27 06:12:10 +0000

    Firest Shell Script

---
+++ Shell-Script-Project/ShellScript-01.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+##############################################
+# This is my first Shell Scripting file
+#
+# Name: Mani Kevala
+# Date: 27-June-2025
+#
+# File Number: 01
+#
+# ############################################
+# 
+# To see the executing commands
+set -x
+
+# If error the executing process will stop 
+set -e
+
+
+# Welcome Meaage 
+echo "Hello this is the first Schell Script file"
+
+# Check the desh usage
+df -h
+
+# Check the memory usage 
+free -h
+
+# list the files and folders 
+ls 
+
