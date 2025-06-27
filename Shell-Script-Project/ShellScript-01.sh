ca27a8033fdbe31612e9b53f6b83e56efc1c1dfe
Mani Kevala <manikevala2001@gmail.com>
2025-06-27 06:29:46 +0000

    First shell file updated

--- Shell-Script-Project/ShellScript-01.sh
+++ Shell-Script-Project/ShellScript-01.sh
@@ -16,9 +16,13 @@ set -x
 # If error the executing process will stop 
 set -e
 
+# Ask user name
+echo "Enter Your Name"
+
+read "NAME"
 
 # Welcome Meaage 
-echo "Hello this is the first Schell Script file"
+echo "Hello "$NAME", this my the first Shell Script file"
 
 # Check the desh usage
 df -h
